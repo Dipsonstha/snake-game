7d42f9b9356ec735e3fe6a9f125e151da79d059c
Dipsonstha <dipson9861@gmail.com>
2024-01-04 10:34:26 +0545

    Update index.js
    added some functionalities for the snake to move

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 //Define Html element
 
 const board = document.getElementById("game-board");
+
 //Define game variables
+
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }]; //setting/initiliazing  the snake to the center
+let food = generateFood();
+let direction = "right";
+let gameInterval;
+
 //draw game map, snake, food
 
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
 //Draw Snake
@@ -40,3 +48,55 @@ function setPosition(element, position) {
 
 //Testing draw func
 draw();
+
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  //appending the acutal food to the board
+  board.appendChild(foodElement);
+}
+function generateFood() {
+  //generates random number from 0-1 eg:0.321,0.4,0.99999
+  //.floor to generate an whole number
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+//Moving the snake
+function move() {
+  // creates a new object (head) by using the spread syntax ({...}) to copy the properties of the first element of the snake array into the new object.
+  const head = { ...snake[0] }; //copy of original array
+  switch (direction) {
+    case "up":
+      head.y--; //it starts from 10 wo we need to go down in numbers to go up
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "left":
+      head.x--;
+      break;
+  }
+  snake.unshift(head); //add head object at the start of snake array
+
+  //if not then the snake just grows only
+  //snake.pop(); //to move the snake
+  if (head.x === food.x && head.y === food.y) {
+    food = generateFood();
+    clearInterval(); //clear past interval
+    gameInterval = setInterval(() => {
+      move();
+      draw();
+    });
+  }
+}
+
+//Test moving
+// setInterval(() => {
+//   move(); //Move first
+//   draw(); //Then draw again new positin
+// }, 200);
